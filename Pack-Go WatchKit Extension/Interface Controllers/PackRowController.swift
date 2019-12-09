07fbd6897225cd70e831616d52234a5cfa12df48
Laurence C <34558191+chongla@users.noreply.github.com>
2019-12-09 14:54:25 -0500

    Added Classes for WatchOS connectivity Preparation

---
+++ Pack-Go WatchKit Extension/Interface Controllers/PackRowController.swift
@@ -0,0 +1,15 @@
+//
+//  PackRowController.swift
+//  Pack-Go WatchKit Extension
+//
+//  Created by Lorraine Chong on 2019-12-09.
+//  Copyright © 2019 Xcode User. All rights reserved.
+//
+
+import WatchKit
+
+class PackRowController: NSObject {
+    @IBOutlet var lblName : WKInterfaceLabel!
+    @IBOutlet var lblType : WKInterfaceLabel!
+    @IBOutlet var imgPack : WKInterfaceImage!
+}
